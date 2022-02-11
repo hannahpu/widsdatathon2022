8a2dc1063cfe14b5fbe3cdcbf4b96704ce4b5bfe
Diana W <windemut@uw.edu>
2022-02-11 12:44:20 -0800

    update performance eval plots

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -147,3 +148,30 @@ def plot_rmse_by_group(
         axes[i + 1].set_yticklabels([])
         axes[i + 1].set_ylabel("")
     return
+
+
+def add_median_labels(ax, precision=".1f"):
+    # https://stackoverflow.com/questions/38649501/labeling-boxplot-in-seaborn-with-median-value
+    lines = ax.get_lines()
+    boxes = [c for c in ax.get_children() if type(c).__name__ == "PathPatch"]
+    lines_per_box = int(len(lines) / len(boxes))
+    for median in lines[4 : len(lines) : lines_per_box]:
+        x, y = (data.mean() for data in median.get_data())
+        # choose value depending on horizontal or vertical plot orientation
+        value = x if (median.get_xdata()[1] - median.get_xdata()[0]) == 0 else y
+        text = ax.text(
+            x,
+            y,
+            f"{value:{precision}}",
+            ha="center",
+            va="center",
+            fontweight="bold",
+            color="white",
+        )
+        # create median-colored border around white text for contrast
+        text.set_path_effects(
+            [
+                path_effects.Stroke(linewidth=3, foreground=median.get_color()),
+                path_effects.Normal(),
+            ]
+        )
